fd2a411471a059ed7cf48a7a95342ad0ad28b216
hernieaw <nws05_01084680@pb.edu.bn>
2018-02-18 21:39:27 +0800

    Firebase

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCLggTuaED0gLtsnEDRvdmJFgsZIXsCLko",
+    authDomain: "foodex-mwc-nws05.firebaseapp.com",
+    databaseURL: "https://foodex-mwc-nws05.firebaseio.com",
+    projectId: "foodex-mwc-nws05",
+    storageBucket: "foodex-mwc-nws05.appspot.com",
+    messagingSenderId: "861053058020"
+}
