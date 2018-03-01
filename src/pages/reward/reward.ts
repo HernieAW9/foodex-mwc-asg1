184dbba004d43817692ee005b734a18fc44a691f
hernieaw <nws05_01084680@pb.edu.bn>
2018-03-01 11:25:32 +0800

    Added alert function on buttons

--- src/pages/reward/reward.ts
+++ src/pages/reward/reward.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RewardPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,62 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RewardPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  redeemOpen: boolean;
+  redeemResult;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RewardPage');
+  }
+
+  showQR() {
+    let confirm = this.alertCtrl.create({
+      title: 'Open Scanner?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Okay clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  showRedeem() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Redeem option');
+
+    alert.addInput({
+      type: 'radio',
+      label: '160 points: Discount 5%',
+      value: 'disc',
+      checked: true
+    });
+    alert.addInput({
+      type: 'radio',
+      label: '300 points: Voucher $10 off',
+      value: 'voucher',
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.redeemOpen = false;
+        this.redeemResult = data;
+      }
+    });
+    alert.present();
   }
 
 }
