9c2046f735b37df78496f78ae23bae07311afdae
Zawanah <nws05_01047367@pb.edu.bn>
2018-03-01 14:42:18 +0800

    zaw update  belait html and temb html

---
+++ src/pages/temburong/temburong.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TemburongPage } from './temburong';
+
+@NgModule({
+  declarations: [
+    TemburongPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TemburongPage),
+  ],
+})
+export class TemburongPageModule {}
