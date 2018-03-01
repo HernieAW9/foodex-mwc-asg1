0c916316233119d7186c18b13cf94a2f6384e47e
Kamal <nws05_01089390@pb.edu.bn>
2018-03-01 14:25:23 +0800

    tutong lagi

---
+++ src/pages/mauri/mauri.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MauriPage } from './mauri';
+
+@NgModule({
+  declarations: [
+    MauriPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MauriPage),
+  ],
+})
+export class MauriPageModule {}
