d13621bf1fb55341c3aa81e68be34c349064a073
hernieaw <nws05_01084680@pb.edu.bn>
2018-03-03 15:26:41 +0800

    Implement Reset Password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { KawanPage } from '../pages/kawan/kawan';
 import { NtkPage } from '../pages/ntk/ntk';
 import { MauriPage } from '../pages/mauri/mauri';
 import { MauripixPage } from '../pages/mauripix/mauripix';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
 
 @NgModule({
   declarations: [
@@ -68,7 +69,8 @@ import { MauripixPage } from '../pages/mauripix/mauripix';
     KawanPage,
     NtkPage,
     MauriPage,
-    MauripixPage
+    MauripixPage,
+    ResetPasswordPage
   ],
   imports: [
     BrowserModule,
@@ -107,7 +109,8 @@ import { MauripixPage } from '../pages/mauripix/mauripix';
     KawanPage,
     NtkPage,
     MauriPage,
-    MauripixPage
+    MauripixPage,
+    ResetPasswordPage
   ],
   providers: [
     StatusBar,
