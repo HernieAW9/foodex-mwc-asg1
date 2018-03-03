dd5301e6d5ba4fb0593f970a9680b6ec57d721a5
Zawanah <nws05_01047367@pb.edu.bn>
2018-03-03 15:01:05 +0800

    temburong lagi

--- src/pages/temburong/temburong.ts
+++ src/pages/temburong/temburong.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
 
 /**
  * Generated class for the TemburongPage page.
@@ -8,8 +7,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-=======
->>>>>>> 86f263b3c2b1ccaaa1a958c1ca6a0f79429684a6
 import { A9Page } from '../a9/a9';
 import { KawanPage } from '../kawan/kawan';
 import { NtkPage } from '../ntk/ntk';
