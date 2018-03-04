649aff364202d28d28b836b1e33cc37215a4720e
hernieaw <nws05_01084680@pb.edu.bn>
2018-03-04 23:28:39 +0800

    Editted TemburongPage

--- src/pages/temburong/temburong.ts
+++ src/pages/temburong/temburong.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TemburongPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-import { A9Page } from '../a9/a9';
 import { KawanPage } from '../kawan/kawan';
 import { NtkPage } from '../ntk/ntk';
 
@@ -24,10 +16,6 @@ export class TemburongPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TemburongPage');
-  }
-
-  goToA9(){
-    this.navCtrl.push(A9Page);
   }
 
   goToKawan() {
