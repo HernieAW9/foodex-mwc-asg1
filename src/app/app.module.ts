703b1b56dccfe8a72b4413c847b643a0943ea1b2
hernieaw <nws05_01084680@pb.edu.bn>
2018-03-04 23:31:18 +0800

    Fixed error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,6 @@ import { IdealpixPage } from '../pages/idealpix/idealpix';
 import { IdealmenuPage } from '../pages/idealmenu/idealmenu';
 import {TutongPage } from '../pages/tutong/tutong';
 import { TemburongPage } from '../pages/temburong/temburong';
-import { A9Page } from '../pages/a9/a9';
 import { KawanPage } from '../pages/kawan/kawan';
 import { NtkPage } from '../pages/ntk/ntk';
 import { MauriPage } from '../pages/mauri/mauri';
@@ -88,7 +87,6 @@ import { NtkmenuPage } from '../pages/ntkmenu/ntkmenu';
     SearchPage,
     TutongPage,
     TemburongPage,
-    A9Page,
     KawanPage,
     NtkPage,
     MauriPage,
@@ -148,7 +146,6 @@ import { NtkmenuPage } from '../pages/ntkmenu/ntkmenu';
     SearchPage,
     TutongPage,
     TemburongPage,
-    A9Page,
     KawanPage,
     NtkPage,
     MauriPage,
